76f2ea8f61c9ec70ff207485d9e5178abaaee4de
Riccardo <riccardoori281@gmail.com>
2026-06-19 13:07:47 -0400

    redid to refresh

--- include/unique_ptr.hpp
+++ include/unique_ptr.hpp
@@ -1,64 +1,95 @@
 #include <cstddef>
-#include <type_traits>
 #include <utility>
-
 namespace ricc{
-		
-		template<typename T>
-		class unique_ptr{
-				private:
-						T* ptr;
-				public:
-						
-						// constructor
-						unique_ptr():ptr(nullptr){}
-						// constructor by assignment
-						unique_ptr &operator= (unique_ptr &other)  = delete;	
-						
-						// constructor by copy
-						unique_ptr(unique_ptr &other) = delete;
-						
-						// construcotr with parameter
-						unique_ptr(T* p): ptr(p){}
 
-						//operator bool (checks whether or not it owns an object)
-						operator bool(){
-								return ptr != nullptr;
-						}
-						
-						// access unerlying ptr
-						T* get(){
-								return ptr;
-						}
-						// reset ptr
-						void reset(T* other){
-								if (other != nullptr){
-										delete_underlying_ptr();
-										std::exchange(ptr,other);
-								}	
-						}
+template<typename T>
+class unique_ptr{
 
-						//helper function	
-						void delete_underlying_ptr(){
-								if (ptr != nullptr){
-										delete ptr;
-										ptr = nullptr;
-								}
-						}
+		private:
+				T* ptr;
+
+		public:
+				//constructor
+				unique_ptr(){unique_ptr(nullptr);};
+
+				// copy constructable removed
+				unique_ptr(unique_ptr& p) = delete;
+
+				// copy assignable removed
+				unique_ptr& operator = (const unique_ptr&) = delete;	
+
+				//moving constructor
+				unique_ptr(unique_ptr && other):ptr{nullptr}
+				{
 						
-						T& operator*(){return (*ptr);}	
-		};	
-		
+						std::swap(ptr,other.ptr);
 						
-}
-//non member function
-template<typename T1,typename T2>
-bool operator==(ricc::unique_ptr<T1>& x, ricc::unique_ptr<T2>& y){
-		return x.get() == y.get();
-}
+				}
+
+				// moving assignment
+				unique_ptr & operator = (unique_ptr && other){
+						if(ptr == other){
+								return *this;	
+						} 
+						delete ptr;
+						ptr = other.ptr;
+						other.ptr = nullptr;
+						return *this;
+				}
+
+				// parameterized constructor
+				unique_ptr(T* t):ptr{t}{};
+
+				T* get(){
+						return ptr;
+				}	
+				T* release(){
+						T* temp = ptr;
+						ptr = nullptr;
+						return temp;
+				};
+
+				T& operator*(){
+						return *ptr;
+				}
+				
+				void reset(T* other){
+						delete ptr;
+						std::swap(ptr,other);		
+				}
+			
+};
+
+template<typename T1, typename T2>
+bool operator==( ricc::unique_ptr<T1>& x, ricc::unique_ptr<T2>& y ){
+		return x.get() == y.get();	
+};
 template<typename T1>
-bool operator==(ricc::unique_ptr<T1>& x,std::nullptr_t y){
-		return x.get() == nullptr;
+bool operator==(ricc::unique_ptr<T1>&x, std::nullptr_t p){
+		return x.get() == p;
 }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- main.cpp
+++ main.cpp
@@ -2,32 +2,29 @@
 #include <assert.h>
 #include "unique_ptr.hpp"
 #include <memory>
-
-
+#include <type_traits>
 
 int main(){
 		
-		ricc::unique_ptr<int> p1(new int(10));			
-		ricc::unique_ptr<int> p2;
-		assert(p1);
-		assert(!p2);
+					
+		ricc::unique_ptr<int> p2 ;
+		ricc::unique_ptr<int> p1 = new int(10);	
 		
-		int* x = new int(10);
-		ricc::unique_ptr<int> ptr(x);
-		int* rawptr = ptr.get();
-		assert(*rawptr == 10);	
-		assert(rawptr == x);
+		
+		assert(*p1.get() == 10);
+		int *x = p1.release();
+		assert(x != nullptr);
+		assert(p1 == nullptr);
+		
+		assert(std::is_move_assignable_v<ricc::unique_ptr<int>>);
+		assert(std::is_move_constructible_v<ricc::unique_ptr<int>>);	
+		assert(!std::is_copy_constructible_v<ricc::unique_ptr<int>>);
+		assert(!std::is_copy_assignable_v<ricc::unique_ptr<int>>);
 
-		ricc::unique_ptr<int> p3(new int(10));
-		p3.reset(new int(20));
+		ricc::unique_ptr<int>p3 = new int(10);	
+		p3.reset(new int (20));
 		assert(*p3 == 20);
-		
-		ricc::unique_ptr<int> p5(new int(10));
-		ricc::unique_ptr<int> p6(new int(10));
-		
-		ricc::unique_ptr<int> p0;
-		assert(p5 == p5);
-		assert(p0 == nullptr);
+			
 
 
 		return 0;
