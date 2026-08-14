b86d2b1c6caa9112824cffe24a0cbcc269393f46
Riccardo <riccardoori281@gmail.com>
2026-08-14 13:10:55 -0400

    added assign

--- include/list.hpp
+++ include/list.hpp
@@ -1,107 +1,106 @@
+#include <initializer_list>
+#include <stdexcept>
 #include <utility>
 #include <iostream>
 
 namespace ricc{
 
-template <typename T>
+
+template<typename T>
 class forward_list{
 
 private:
 
+		using reference = T&;
+
 struct ListNode{
 
 		T value;
-		ListNode* next;
+		ListNode* next{nullptr};
 
-		ListNode(const T& value): value(value)
+		ListNode() = default; 
+
+		ListNode(const T& value):value(value)
 		{
 		}
 
-		ListNode(T&& value): value(std::move(value))
+		ListNode(T&& value):value(std::move((value)))
 		{
 		}
-
 };
 
-ListNode* head{nullptr};
 std::size_t size_{};
+ListNode* head{nullptr};
 
-void swap(forward_list& temp){
-
-		std::swap(head,temp.head);
+constexpr void swap(forward_list& temp){
+		
 		std::swap(size_,temp.size_);
+		std::swap(head,temp.head);
+
 }
+
+constexpr void clear(){
+
+		for (auto it{head}; it != nullptr;){
+				auto old = it->next;
+				delete it;
+				it = old;
+		}
+		size_ = 0;
+}
+
 
 public:
 
-forward_list() = default;	
+forward_list() = default;
 
+// destructor
 ~forward_list(){
 
-		if (head != nullptr){
-
-				for (ListNode* current{head}; current != nullptr;){
-
-						std::cout << "Deleted" << std::endl;
-						auto next = current->next;
-						delete current;
-						current = next;
-
-				}
-
-		}
-		size_ = 0;
-
+		clear();
+		
 }
-
-/*
- * Copy constructor
- * */
 
 forward_list(const forward_list& other):size_(other.size_){
 
-		
 		ListNode* current = other.head->next;
 		head = new ListNode(other.head->value);
 		ListNode* iterator{head};
-		while (current != nullptr){
-				ListNode* node = new ListNode(current->value);
-				iterator->next = node;
+
+		while(current != nullptr){
+
+				ListNode* newNode = new ListNode(current->value);
+				iterator->next = newNode;
 				current = current->next;
 				iterator = iterator->next;
+
 		}
-		std::cout << "Copy constructor" << std::endl;
-
+		std::cout << "Copy constructed" << std::endl;
 }
 
-/*
- * Move constructor
- * */
-forward_list (forward_list&& other) noexcept: size_(std::exchange(other.size_,0)),head(std::exchange(other.head,nullptr)){
+forward_list& operator= (const forward_list& other){
 
-		std::cout << "Move constructor" << std::endl;
-}
-
-/* Copy assignment */
-forward_list& operator = (const forward_list& other){
-
-		std::cout << "Copy assignment" << std::endl;
 		if (this == &other){
 
 				return *this;
 		}
 
 		forward_list temp(other);
+
 		swap(temp);
 
 		return *this;
+
+} 
+
+forward_list (forward_list&& other) noexcept :size_(std::exchange(other.size_,0)),head(std::exchange(other.head,nullptr)  )
+{
+		std::cout << "Move constructor" << std::endl;
 }
 
-/* Move assingment */
 forward_list& operator= (forward_list&& other) noexcept {
 
 		std::cout << "Move assignment" << std::endl;
-
 		if (this == &other){
 
 				return *this;
@@ -114,37 +113,74 @@ forward_list& operator= (forward_list&& other) noexcept {
 		return *this;
 }
 
-void reverse() noexcept {
+
+void assign(std::size_t count, const T& value){
+
+		clear();
+
+		if (count <= 0){
+
+				throw std::range_error("Invalid size");
+		}
+
+		ListNode* dummy = new ListNode();
+		dummy ->next = head;
+		ListNode* iterator = dummy;
+
+		for (std::size_t i{}; i < count; ++i){
+
+				ListNode* newNode = new ListNode(value);
+				iterator->next = newNode;
+				iterator = iterator->next;
+		}
+
+		head = dummy->next;
+
+		delete dummy;
+}
+
+template <typename U>
+void push_front(U&& value){
+
+		ListNode* oldhead = head;
+		ListNode* newNode = new ListNode(std::forward<U>(value));
+		newNode->next = oldhead;
+		head = newNode;
+		size_++;
+}
+
+constexpr void reverse(){
 
 		ListNode* node{nullptr};
+		ListNode* current{head};
 
-		while (head != nullptr){
-				
-				ListNode* old = head->next;
-				head->next = node;
-				node = head;
-				head = old;
+		while (current != nullptr){
+
+				ListNode* temp = current->next;
+
+				current->next = node;
+				node = current;
+				current = temp;
 		}
+
 		head = node;
 
 }
 
-bool empty() const noexcept {
+constexpr reference front(){
 
-		return size_ == 0;
+		return head->value;
 }
 
-template<typename U>
-void push_front(U&& value){
 
-		ListNode* oldhead = head;
+void Print(){
 
-		ListNode* newNode = new ListNode(std::forward<U>(value));
+		ListNode* current = head;
+		while (current != nullptr){
 
-		newNode -> next = oldhead;
-
-		++size_;
-		head = newNode;
+				std::cout << current->value << std::endl;
+				current = current->next;
+		}
 }
 
 constexpr std::size_t size(){
@@ -152,28 +188,23 @@ constexpr std::size_t size(){
 		return size_;
 }
 
-void print(){
+constexpr void popFront(){
 
-		ListNode* current = head;
-
-		while (current != nullptr){
-
-				std::cout << current->value << std::endl;
-
-				current = current->next;
-		}
-		
+		ListNode* old = head->next;
+		delete head;
+		head = old;
+		--size_;
 }
 
-constexpr T& front(){
 
-		return head->value;
-}
+
 
 };
 
 
-};
+}
+
+
 
 
 
--- list.cpp
+++ list.cpp
@@ -8,17 +8,30 @@ int main(){
 		letters.push_front('a');
 
 		assert(letters.front() == 'a');
-		letters.push_front('b');
-		assert(letters.front() != 'a');
 
-/*
-		b.print();
+		ricc::forward_list<char> letters1{letters};
+		assert(letters1.front() == 'a');
 
-		b.reverse();
-		b.print();
-*/
+		ricc::forward_list<char> letters2{};
+		letters2 = letters;
+		assert(letters2.front() == 'a');
 
+		ricc::forward_list<char> letters3{};
+		letters3 = std::move(letters);
+		assert(letters3.front() == 'a');
 
+		letters3.push_front('b');
+		assert(letters3.front() == 'b');
+
+		letters3.reverse();
+		assert(letters3.front() == 'a');
+
+		letters3.popFront();
+		assert(letters3.front() == 'b');
+		assert(letters3.size() == 1);
+
+		letters3.assign(2, 'a');
+		letters3.Print();
 
 		return 0;
 }
